541602e82dc88799a76cb5fdf4cfd0498fa9e978
Vladimir Kidyaev <krios2146@gmail.com>
2023-10-04 13:55:45 +0700

    feat: implement function that reads words from the .txt file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+use std::fs::File;
 use std::io;
+use std::io::BufRead;
+
+use rand::Rng;
 
 fn main() {
     const MAX_MISTAKES: i32 = 6;
@@ -33,9 +37,22 @@ fn main() {
     }
 }
 
-// TODO: should return random word from /resources/dictionary.txt
 fn choose_word_for_game() -> String {
-    "world".to_string()
+    let file = File::open("resources/dictionary.txt").expect("Cannot open dictionary.txt");
+    let reader = io::BufReader::new(file);
+
+    let mut words = Vec::new();
+    for line in reader.lines() {
+        if let Ok(line) = line {
+            words.push(line)
+        }
+    }
+
+    let mut rng = rand::thread_rng();
+    let random_index = rng.gen_range(0..words.len());
+
+    let random_word = &words[random_index];
+    return random_word.to_string();
 }
 
 fn print_hangman(mistakes: i32) {
